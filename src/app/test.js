045c6046087f64e28b6c3c6474304b1087e11925
Vi Tiet <vitiet.programmer@gmail.com>
2025-01-24 13:09:10 -0500

    update project

---
+++ src/app/test.js
@@ -0,0 +1,20 @@
+import { Canvas } from '@react-three/fiber'
+import { XR, createXRStore } from '@react-three/xr'
+import { useState } from 'react'
+
+export default function testFunc() {
+  const [red, setRed] = useState(false)
+  return (
+    <>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Canvas>
+        <XR store={store}>
+          <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
+            <boxGeometry />
+            <meshBasicMaterial color={red ? 'red' : 'blue'} />
+          </mesh>
+        </XR>
+      </Canvas>
+    </>
+  )
+}
