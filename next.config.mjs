e7f35ed7389bf1b1a9008a4f613a49268e1829aa
Vi Tiet <vitiet.programmer@gmail.com>
2025-01-28 11:57:40 -0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: "/(.*)", // Apply to all routes
-          headers: [
-            {
-              key: "Content-Security-Policy",
-              value: "script-src 'self' https://cdn.delight-vr.com;",
-            },
-          ],
-        },
-      ];
-    },
-  };
+const nextConfig = {};
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     async headers() {
+//       return [
+//         {
+//           source: "/(.*)", // Apply to all routes
+//           headers: [
+//             {
+//               key: "Content-Security-Policy",
+//               value: "script-src 'self' https://cdn.delight-vr.com;",
+//             },
+//           ],
+//         },
+//       ];
+//     },
+//   };
   
-  export default nextConfig;
+//   export default nextConfig;
   
